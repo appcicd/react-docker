3ec79d251fe188b638c19aab39c7da5cf10e9bd0
appcicd <mb@belai.us>
2019-02-03 14:43:16 -0800

    text modified

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Docker</h1>
         </header>
-        <p className="App-intro">I was changed on the Feature branch</p>
+        <p className="App-intro">I was changed on the Feature branch </p>
       </div>
     );
   }
